0f8ad1b7be41c3bb20940ff0476e5c6e561f9160
stephen_adah <mailstephenreal@gmail.com>
2023-12-20 10:58:24 -0800

    models/state.py

--- models/state.py
+++ models/state.py
@@ -10,11 +10,12 @@ import shlex
 
 
 class State(BaseModel, Base):
-    """ State class 
+    """ State class
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan', backref="state")
+    cities = relationship(
+            "City", cascade='all, delete, delete-orphan', backref="state")
 
     @property
     def cities(self):
