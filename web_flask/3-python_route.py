ce82837c00e0a3f729d7080203d5a0df104e9a75
stephen_adah <mailstephenreal@gmail.com>
2024-01-29 00:50:46 +0100

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,7 +28,7 @@ def c_route(text):
     return 'C {}'.format(formatted_text)
 
 
-@app.route('/python/', defaults={'text':'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text):
     """display “Python ”, followed by the value of the text variable"""
